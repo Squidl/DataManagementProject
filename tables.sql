36b0621727c704d4143d963fd6f7a617d85052ba
Squidl <demos.sacha@gmail.com>
2015-04-21 18:41:32 -0400

    debug

--- tables.sql
+++ tables.sql
@@ -19,18 +19,36 @@ CREATE TABLE Job(
        client int NOT NULL REFERENCES Client,
      location varchar(400),
     scheduled timestamp,
-        payed money,
- photographer int REFERENCES Photographer
+ photographer int REFERENCES Photographer,
+    finalized bool NOT NULL DEFAULT FALSE,
+        CHECK ( finalized OR photographer IS NOT NULL )
 );
 
 CREATE TABLE EventJob(
-       assistant int REFERENCES Photographer
+     assistant int REFERENCES Photographer
 ) INHERITS (Job);
 
 CREATE TABLE Photo(
-       proof_id serial PRIMARY KEY,
-       expiration timestamp NOT NULL DEFAULT now() + interval '6 months',
-       cost money NOT NULL,
-       is_ordered bool NOT NULL DEFAULT false,
-       from_job int NOT NULL REFERENCES Photo
+      proof_id serial PRIMARY KEY,
+    expiration timestamp NOT NULL DEFAULT now() + interval '6 months',
+          cost money NOT NULL,
+    is_ordered bool NOT NULL DEFAULT false,
+      from_job int NOT NULL REFERENCES Photo
+);
+
+CREATE TABLE Package(
+    package_id serial PRIMARY KEY,
+   description varchar(4000) NOT NULL
+);
+
+CREATE TABLE PhotoType(
+  phototype_id serial PRIMARY KEY,
+     photoname varchar(150) NOT NULL,
+        height numeric NOT NULL,
+         width numeric NOT NULL
+);
+
+CREATE TABLE PhotoInPackage(
+       package int NOT NULL REFERENCES Package,
+         photo int NOT NULL REFERENCES Photo
 );
--- views.sql
+++ views.sql
@@ -15,3 +15,14 @@ SELECT photo.proof_id AS "proof#"
   FROM Photo p, Job j
  WHERE p.from_job=j.job_id
    AND expiration < now()
+
+CREATE VIEW PhotoPackages AS
+  WITH PhotoDesc AS (
+       SELECT pt.phototype_id AS phototype_id
+       	    , concat( str "-", pt.photoname, str " (", pt.width, str "'x" ,pt.height , str "')")
+         FROM PhotoType as pt
+     )
+SELECT 
+  FROM Package p, PhotoDesc pt, PhotoInPackage pinpt
+ WHERE p.package_id = pinpt.package
+   AND pinpt.photo = pt.phototype_id
