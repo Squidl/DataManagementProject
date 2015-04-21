d8a698c43c3549ab8b189b62c367735a1a64fccf
Squidl <demos.sacha@gmail.com>
2015-04-21 11:55:12 -0400

    added view

--- views.sql
+++ views.sql
@@ -3,10 +3,15 @@ SELECT (SELECT SUM(Photo.cost) - Job.payed
           FROM Photo
 	 WHERE Photo.from_job=Job.job_id
        ) as balance
-     , Client.full_name as customer
-     , Client.phone as contact
+     , Client.full_name AS customer
+     , Client.phone AS contact
   FROM Job, Client
  WHERE Job.Client=Client.client_id;
 
 CREATE VIEW disposable AS
-SELECT 
+SELECT photo.proof_id AS "proof#"
+     , photo.expiration AS "exp since"
+     , job.scheduled AS "taken on"
+  FROM Photo p, Job j
+ WHERE p.from_job=j.job_id
+   AND expiration < now()
